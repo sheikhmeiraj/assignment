ce2d0f2d80547adb79c64cc06fcbadecd4a40899
sheikh meiraj <100143061+sheikhmeiraj@users.noreply.github.com>
2026-05-05 16:49:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  preview: {
+    allowedHosts: [
+      'assignment-production-7399.up.railway.app'
+    ]
+  }
+})
